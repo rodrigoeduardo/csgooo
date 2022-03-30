cbcde0fb74e92822a331082aa5290594b07b41bd
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2022-03-30 08:40:08 -0300

    page title added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { theme } from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>csgooo</title>
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
